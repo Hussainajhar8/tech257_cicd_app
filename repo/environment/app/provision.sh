b11135ed05175d8e069fbb1c0e48b2b45402752d
Miskeen <hussainajhar8@gmail.com>
2024-03-21 15:30:17 +0000

    fixed provision script in environement

--- repo/environment/app/provision.sh
+++ repo/environment/app/provision.sh
@@ -6,28 +6,28 @@ sudo apt-get update -y
 # upgrade any packages available
 sudo apt-get upgrade -y
 
+
 # install git
 sudo apt-get install git -y
 
-sudo apt-get install nginx -y
-
-sudo service nginx restart
-sudo service nginx enable
-
 # install nodejs
-sudo apt-get install python-software-properties -y
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install nodejs -y
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs
 
-# reload nginx
-sudo systemctl reload nginx
+# install express
+sudo npm install express
+sudo npm install mongoose
+sudo npm install ejs
 
 # install pm2
-sudo apt install npm -y
-
 sudo npm install pm2 -g
 
-# remove the old file and add our one
-#sudo rm /etc/nginx/sites-available/default
-#sudo cp /home/ubuntu/sre_jenkins_cicd/environment/app/nginx.default /etc/nginx/sites-available/default
+sudo apt-get install nginx -y
 
+# remove the old file and add our one
+sudo rm /etc/nginx/sites-available/default
+sudo cp /home/ubuntu/app/environment/app/nginx.default /etc/nginx/sites-available/default
+
+# finally, restart the nginx service so the new config takes hold
+sudo service nginx restart
+sudo service nginx enable
