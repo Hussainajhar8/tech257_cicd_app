42f62702f766e2cc5fa440865ae8959393d6637c
Miskeen <hussainajhar8@gmail.com>
2024-03-21 15:39:17 +0000

    fixed provision script in environement

--- repo/environment/app/provision.sh
+++ repo/environment/app/provision.sh
@@ -11,13 +11,10 @@ sudo apt-get upgrade -y
 sudo apt-get install git -y
 
 # install nodejs
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs
-
-# install express
-sudo npm install express
-sudo npm install mongoose
-sudo npm install ejs
+sudo apt-get install python-software-properties -y
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
 
 # install pm2
 sudo npm install pm2 -g
@@ -25,9 +22,9 @@ sudo npm install pm2 -g
 sudo apt-get install nginx -y
 
 # remove the old file and add our one
-sudo rm /etc/nginx/sites-available/default
-sudo cp /home/ubuntu/app/environment/app/nginx.default /etc/nginx/sites-available/default
+#sudo rm /etc/nginx/sites-available/default
+#sudo cp /home/ubuntu/sre_jenkins_cicd/environment/app/nginx.default /etc/nginx/sites-available/default
 
 # finally, restart the nginx service so the new config takes hold
-sudo service nginx restart
-sudo service nginx enable
+#sudo service nginx restart
+#sudo service nginx enable
